61fa104d86c04e8d6f7ca6de841ba59073c8f901
molinajimenez <molinajimenez01@gmail.com>
2018-05-12 22:31:24 -0600

    *CALIFICAR ESTE COMMIT*

---
+++ Assets/VirtualButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+public class VirtualButton : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public Animator anim;
+    VirtualButtonBehaviour virtualButton;
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        anim.SetTrigger("sing");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        
+    }
+
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+        virtualButton = GetComponent<VirtualButtonBehaviour>();
+        virtualButton.RegisterEventHandler(this);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
